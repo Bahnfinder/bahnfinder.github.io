24251c19cfa7e5f0cb9104619b845307f53eb25f
emilian <89602269+etsde@users.noreply.github.com>
2023-11-24 21:05:22 +0100

    tailwind-works

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './docs/**/*.html',   // Pfade zu HTML-Dateien im docs-Ordner
+    './docs/**/*.css',     // Pfade zu CSS-Dateien im src-Ordner
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
